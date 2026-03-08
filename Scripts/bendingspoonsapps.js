fbb51caeb4e6f411a8a02528a8fa02d271a6fa5e
Ah Kei <okmake@gmail.com>
2026-03-08 18:32:44 +0800

    Update bendingspoonsapps.js

--- Scripts/bendingspoonsapps.js
+++ Scripts/bendingspoonsapps.js
@@ -1,15 +1,13 @@
-/*
 #!name=Unlock Bending Spoons Apps
 #!desc=Unlocked Remini, Filmicpro, Splice, ThirtydayFitness, Sleep, Focos, Focoslive, Yoga, Firstlight, Doubletake, AlightMotion
 [MITM]
 hostname= %APPEND% *.oracle.bendingspoonsapps.com
 [Script]
-Bendingspoonsapps = type=http-response,pattern=^https:\/\/.+\.oracle\.bendingspoonsapps\.com\/v2\/(purchases\/verify\/apple|purchases\/sign\/apple|users\/setup|users\/legal)$,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/TwoSim/QX-M/refs/heads/main/Scripts/bendingspoonsapps.js,script-update-interval=-1
+Bendingspoonsapps = type=http-response,pattern=^https:\/\/.+\.oracle\.bendingspoonsapps\.com\/v2\/(purchases\/sign\/apple|purchases\/verify\/apple|users\/setup|users\/legal)$,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/TwoSim/QX-M/refs/heads/main/Scripts/bendingspoonsapps.js,script-update-interval=-1
 
 
 let body = $response.body.replace(/\"__is_free__\":false/, "\"__is_free__\":true");
 $done({body});
-*/
 
 function findUrl(_reg) {
   if (_reg.test($request.url)) {
